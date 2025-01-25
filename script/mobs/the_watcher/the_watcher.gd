97657ac7a2c28afe9c8bcea815513fabe3add52a
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-01-25 23:37:13 +0100

    Made The Watcher not attack immediately
    The Watcher now leaves after some time

--- script/mobs/the_watcher/the_watcher.gd
+++ script/mobs/the_watcher/the_watcher.gd
@@ -1,7 +1,11 @@
 extends Object
 class_name TheWatcher
 
-const SPAWN_CHANCE: float = 0.1
+const SPAWN_CHANCE: float = 1.0
+const REACT_TIME: float = 1.0
 const ACTIVE_DURATION: float = 5
+
+const MIN_SPAWN_DELAY: float = 0.5
+const MAX_SPAWN_DELAY: float = 1.5
 
 var isActive: bool = false
--- script/mobs/the_watcher/window.gd
+++ script/mobs/the_watcher/window.gd
@@ -3,7 +3,7 @@ extends RoomElement
 @onready var watcherSpawnPos: Vector3 = %TheWatcherInsideSpawn.global_position
 @onready var watcherRaycast: TheWatcherRaycast = %TheWatcherRaycast
 @onready var animationPlayer: AnimationPlayer = %AnimationPlayer2
-@onready var watcherTimer: Timer = %TheWatcherTimer
+@onready var watcherTimer: Timer = %TheWatcherActiveTimer
 
 @export var watcherSpawnSounds: Array[AudioStream]
 
@@ -12,22 +12,42 @@ func _ready() -> void:
 	watcherRaycast.saw_player.connect(_on_watcher_see_player)
 	room.room_opened.connect(_watcher_spawn)
 	watcherTimer.wait_time = Game.theWatcher.ACTIVE_DURATION
+	watcherTimer.timeout.connect(_watcher_deactivate)
 
 func _on_room_opened() -> void:
 	if Game.theWatcher.isActive:
 		return
-	if randf() > Game.theWatcher.SPAWN_CHANCE:
-		return
-	_watcher_spawn()
+	if randf() <= Game.theWatcher.SPAWN_CHANCE:
+		_watcher_activate()
 	
+func _watcher_activate() -> void:
+	Game.theWatcher.isActive = true
+	get_tree().create_timer(
+		randf_range(
+			Game.theWatcher.MIN_SPAWN_DELAY,
+			Game.theWatcher.MAX_SPAWN_DELAY
+		)
+	).timeout.connect(_watcher_warn)
+	
+func _watcher_warn() -> void:
+	%SpawnSound.stream = watcherSpawnSounds.pick_random()
+	%SpawnSound.play()
+	get_tree().create_timer(Game.theWatcher.REACT_TIME).timeout.connect(_watcher_spawn)
 
 func _watcher_spawn() -> void:
 	watcherRaycast.enabled = true
 	Game.theWatcher.isActive = true
 	%SpawnSound.stream = watcherSpawnSounds.pick_random()
 	%SpawnSound.play()
-	
+	watcherTimer.start()
+
+func _watcher_deactivate() -> void:
+	watcherRaycast.enabled = false
+	Game.theWatcher.isActive = false
+	%SpawnSound.stream = watcherSpawnSounds.pick_random()
+	%SpawnSound.play()
 
 func _on_watcher_see_player():
+	watcherTimer.stop()
 	animationPlayer.play("open")
 	%EnterSound.play()
