6e4c074aaba70e55c2786acbe323c2f675121327
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-01-25 23:20:01 +0100

    Added watcher spawn sound
    Removed watcher light

--- script/mobs/the_watcher/window.gd
+++ script/mobs/the_watcher/window.gd
@@ -1,10 +1,11 @@
 extends RoomElement
 
 @onready var watcherSpawnPos: Vector3 = %TheWatcherInsideSpawn.global_position
-@onready var watcherLight: Light3D = %TheWatcherViewLight
 @onready var watcherRaycast: TheWatcherRaycast = %TheWatcherRaycast
 @onready var animationPlayer: AnimationPlayer = %AnimationPlayer2
 @onready var watcherTimer: Timer = %TheWatcherTimer
+
+@export var watcherSpawnSounds: Array[AudioStream]
 
 func _ready() -> void:
 	super._ready()
@@ -22,8 +23,10 @@ func _on_room_opened() -> void:
 
 func _watcher_spawn() -> void:
 	watcherRaycast.enabled = true
-	watcherLight.visible = true
 	Game.theWatcher.isActive = true
+	%SpawnSound.stream = watcherSpawnSounds.pick_random()
+	%SpawnSound.play()
+	
 
 func _on_watcher_see_player():
 	animationPlayer.play("open")
