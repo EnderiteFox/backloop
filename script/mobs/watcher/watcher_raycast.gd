fd5388298183bf1cd115b91fbc6522e91f7c2ccd
NotARobot3 <greengamer70110@gmail.com>
2025-01-25 17:59:19 +0100

    Watcher raycasting done

--- script/mobs/watcher/watcher_raycast.gd
+++ script/mobs/watcher/watcher_raycast.gd
@@ -1,18 +1,9 @@
 extends RayCast3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.target_position = Game.player.position
-	if is_seeing_player():
+	target_position = Game.player.global_position - self.global_position
 		
-		
-	
-	
 func is_seeing_player():
 	if get_collider() is Player:
 		return true
