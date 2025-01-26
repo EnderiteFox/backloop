ece82f7010481329cbd548e144fa78fb939e9b68
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-01-26 01:37:33 +0100

    Added The Watcher's model

--- scene/mobs/watcher.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody3D
-
-@onready var watcherRaycast:RayCast3D = %watcher_raycast
-
-func _process(delta: float) -> void:
-	if watcherRaycast.is_seeing_player():
-		Game.player.is_alive = false 
-		
