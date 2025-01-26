51e0c932f5eee0b863359403add01533bf120724
NotARobot3 <greengamer70110@gmail.com>
2025-01-26 14:38:59 +0100

    Watcher animation (kill yourself)

--- script/player/stamina_ui.gd
+++ script/player/stamina_ui.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 	
 func _process(_delta: float) -> void:
 	value = Game.player.stamina
+	
 	if Game.player.exhaustion > 0 && !exhausted:
 		var tween: Tween = get_tree().create_tween()
 		tween.tween_property(self, "modulate", Color.RED, TWEEN_TIME).from(Color.WHITE)
@@ -19,16 +20,27 @@ func _process(_delta: float) -> void:
 			func(): %AnimationPlayer.play("exhausted")
 		)
 		exhausted = true
+		
 	elif exhausted && Game.player.exhaustion <= 0:
 		var tween: Tween = get_tree().create_tween()
 		tween.tween_property(self, "modulate", Color.WHITE, TWEEN_TIME).from(self.modulate)
 		%AnimationPlayer.stop()
 		exhausted = false
-	elif Game.player.stamina == 100 && !staminaFull:
+		
+	elif  Game.player.stamina == 100 && !staminaFull:
 		staminaFull = true
 		var tween: Tween = get_tree().create_tween()
 		tween.tween_property(self, "self_modulate", Color.TRANSPARENT, STAMINA_FULL_TRANSITION)
+		tween.tween_callback(
+			func():
+				tween = null
+		)
+		
 	elif Game.player.stamina < 100 && staminaFull && Game.player.exhaustion <= 0:
 		staminaFull = false
 		var tween: Tween = get_tree().create_tween()
 		tween.tween_property(self, "self_modulate", Color.WHITE, STAMINA_NOT_FULL_TRANSITION)
+		tween.tween_callback(
+			func():
+				tween = null
+		) 
