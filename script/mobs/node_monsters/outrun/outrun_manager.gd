fd0de2dd8044827cb1d9d1e5d1ae77b11d6b48c6
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-01-31 17:20:32 +0100

    Added idea files to gitignore
    Added a function in OutrunManager the rooms in the path

--- script/mobs/node_monsters/outrun/outrun_manager.gd
+++ script/mobs/node_monsters/outrun/outrun_manager.gd
@@ -8,3 +8,11 @@ func _ready() -> void:
 
 func _on_room_opened(_room: Room) -> void:
 	pass
+
+func _get_path_rooms() -> Array[Room]:
+	var rooms: Array[Room] = []
+	var currentRoom: Room = Game.roomGenerator.lastRoomOpened
+	while currentRoom != null:
+		rooms.append(currentRoom)
+		currentRoom = currentRoom.previousRoom
+	return rooms
