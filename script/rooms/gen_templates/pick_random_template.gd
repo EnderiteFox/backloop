bd188a17bab6ba069bf5dc446674e3bb920fb614
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-04-05 23:17:29 +0200

    Add a small furniture generation preset
    Moved generation template scripts from res://scripts to res://generation_templates
    Added a tall plant

--- script/rooms/gen_templates/pick_random_template.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-## Deletes all child nodes except for one
-
-func _ready() -> void:
-	var children: Array[Node] = self.get_children()
-	children.shuffle()
-	for i in range(1, children.size()):
-		children[i].free()
