bd14f025eb3abd61a3d7f9c19c879cc2a7267166
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-06-06 18:47:41 +0200

    Added a command to spawn The Shade

--- player/dev_console/debug_commands.gd
+++ player/dev_console/debug_commands.gd
@@ -11,6 +11,27 @@ func spawn(monster: String) -> void:
 			dev_console.print_console("Spawned Outrun")
 		_:
 			dev_console.print_error_console("Unknown monster: %s" % monster)
+			
+		
+func _spawn_the_shade(room: Room) -> void:
+	if Game.the_shade.spawn(room):
+		dev_console.print_console("The Shade spawned")
+	else:
+		dev_console.print_error_console("Failed to spawn The Shade")
+		
+			
+func spawn_the_shade(_the_shade: String, where: String) -> void:
+	if Game.roomGenerator.rooms.is_empty():
+		dev_console.print_error_console("No room to spawn The Shade in")
+		return
+		
+	match where:
+		"random":
+			_spawn_the_shade(Game.roomGenerator.rooms.pick_random())
+		"last":
+			_spawn_the_shade(Game.roomGenerator.rooms[-1])
+		_:
+			dev_console.print_error_console("Invalid location: %s" % where)
 	
 	
 func give(item: String) -> void:
