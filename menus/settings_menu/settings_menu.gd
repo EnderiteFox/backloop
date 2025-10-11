4de65841e429f0047094c2cd4ef628dc8b303014
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-10-11 14:17:34 +0200

    Allow full keyboard navigation in menus

--- menus/settings_menu/settings_menu.gd
+++ menus/settings_menu/settings_menu.gd
@@ -2,13 +2,20 @@ class_name SettingsMenu
 extends Menu
 
 @onready var back_button: Button = %BackButton
-
-@onready var sensibility_slider: Slider = %SensibilitySlider
-@onready var sensibility_spinbox: SpinBox = %SensibilitySpinBox
+@onready var tab_bar: TabBar = %TabBar
 
 
 func _ready() -> void:
 	back_button.pressed.connect(_on_back_button_pressed)
+	
+	
+func open() -> void:
+	super.open()
+	
+	
+func on_focus() -> void:
+	super.on_focus()
+	tab_bar.grab_focus()
 
 
 func _on_back_button_pressed() -> void:
