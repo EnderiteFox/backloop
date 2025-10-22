068dfa719eb456040e50c8d8c5f71c0c2335cc1b
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-10-22 23:45:50 +0200

    Change the clock time progress animation
    Add a sound effect to clocks

--- furniture/clocks/wall_clock.gd
+++ furniture/clocks/wall_clock.gd
@@ -6,34 +6,55 @@ const TRANSITION: Tween.TransitionType = Tween.TRANS_CUBIC
 const EASING: Tween.EaseType = Tween.EASE_OUT
 const HAND_ANIM_TIME: float = 4.0
 
+const WAIT_AFTER_DOOR: float = 2.0
+const MOVE_SUBDIVISIONS: int = 3
+const SUBDIVISION_WAIT: float = 0.2
+const SUBDIVISION_TIME: float = 0.1
+
 @onready var minute_hand: Node3D = %la_2
 @onready var hour_hand: Node3D = %le_2
 
-func update_time(new_time: int, animate: bool = true) -> void:
-	var minute_rotation: Vector3 = Vector3(
-		minute_hand.rotation.x,
-		minute_hand.rotation.y,
-		(((new_time) % 60) / 60.0) * 2 * PI
-   	)
-	while minute_rotation.z < minute_hand.rotation.z:
-		minute_rotation.z += 2 * PI
-	minute_rotation.z += ADDITIONAL_ROTATIONS * 2 * PI * int(60 / 12.0)
-	var minuteTween: Tween = self.create_tween()
-	minuteTween.tween_property(minute_hand, "rotation", minute_rotation, HAND_ANIM_TIME if animate else 0.0) \
-		.set_trans(TRANSITION) \
-		.set_ease(EASING)
+@onready var tick_low_sound: AudioStreamPlayer3D = %TickLowSound
+@onready var tick_high_sound: AudioStreamPlayer3D = %TickHighSound
 
-	var hour_rotation: Vector3 = Vector3(
-		hour_hand.rotation.x,
-		hour_hand.rotation.y,
-		(new_time / 60.0 / 12.0) * 2 * PI
-	)
-	while hour_rotation.z < hour_hand.rotation.z:
-		hour_rotation.z += 2 * PI
-	hour_rotation.z += ADDITIONAL_ROTATIONS * 2 * PI
-	var hourTween: Tween = self.create_tween()
-	hourTween.tween_property(hour_hand, "rotation", hour_rotation, HAND_ANIM_TIME if animate else 0.0) \
-		.set_trans(TRANSITION) \
-		.set_ease(EASING)
+func update_time(new_time: int, animate: bool = true) -> void:
+	if animate:
+		var minute_rotation_delta: float = (((new_time - displayed_time) % 60) / 60.0) * TAU
+	
+		var minute_tween: Tween = self.create_tween()
+		minute_tween.tween_interval(WAIT_AFTER_DOOR)
+		for i in range(1, MOVE_SUBDIVISIONS):
+			minute_tween.tween_callback(tick_low_sound.play)
+			minute_tween.tween_property(
+				minute_hand, 
+				"rotation:z", 
+				minute_rotation_delta / MOVE_SUBDIVISIONS, 
+				SUBDIVISION_TIME
+			).as_relative().set_trans(Tween.TransitionType.TRANS_CUBIC)
+			minute_tween.tween_interval(SUBDIVISION_WAIT)
+			
+		minute_tween.tween_callback(tick_high_sound.play)
+		minute_tween.tween_property(
+			minute_hand,
+			"rotation:z",
+			minute_rotation_delta / MOVE_SUBDIVISIONS,
+			SUBDIVISION_TIME
+		).as_relative().set_trans(Tween.TransitionType.TRANS_CUBIC)
+			
+		var hour_rotation_delta: float = ((new_time - displayed_time) / 60.0 / 12.0) * TAU
+	
+		var hour_tween: Tween = self.create_tween()
+		hour_tween.tween_interval(WAIT_AFTER_DOOR)
+		for i in range(1, MOVE_SUBDIVISIONS + 1):
+			hour_tween.tween_property(
+				hour_hand, 
+				"rotation:z", 
+				hour_rotation_delta / MOVE_SUBDIVISIONS, 
+				SUBDIVISION_TIME
+			).as_relative().set_trans(Tween.TransitionType.TRANS_CUBIC)
+			hour_tween.tween_interval(SUBDIVISION_WAIT)
+	else:
+		minute_hand.rotation.z = ((new_time % 60) / 60.0) * TAU
+		hour_hand.rotation.z = (new_time / 60.0 / 12.0) * TAU
 
 	super.update_time(new_time, animate)
