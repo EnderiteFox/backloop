c1f0056d6869a5d94cbc688a1d94af2b6ff2a234
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-12-12 22:48:36 +0100

    Add an easter egg in the menu

--- menus/main_menu/main_menu.gd
+++ menus/main_menu/main_menu.gd
@@ -1,5 +1,6 @@
 extends Node
 
+# Title
 const TITLE_MIN_BRIGHTNESS: float = 0.6
 const TITLE_MAX_BRIGHTNESS: float = 1.0
 const TITLE_MIN_BLINK_TIME: float = 0.1
@@ -9,11 +10,19 @@ const TITLE_MAX_BLINK_WAIT_TIME: float = 1.5
 const TITLE_MIN_BLINK_COUNT: int = 5
 const TITLE_MAX_BLINK_COUNT: int = 8
 
+# Easter Egg
+const EASTER_EGG_CHANCE: float = 1.0 / 1000
+const EASTER_EGG_DURATION: float = 3.0
+const EASTER_EGG_AUDIO_LAG_LENGTH: float = 0.227
+var easter_egg_audio_pos: float = 0
+
 @onready var title: Label3D = %Title
 @onready var menu_music_player: AudioStreamPlayer = %MenuMusic
 @onready var play_button: Button = %PlayButton
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var start_room: Room = %StartRoom
+@onready var ui: Control = %UI
+@onready var easter_egg_node: Node3D = %EasterEgg
 
 
 func _ready() -> void:
@@ -48,6 +57,11 @@ func _blink_title() -> void:
 func _on_play_button_pressed() -> void:
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CAPTURED)
 	play_button.disabled = true
+	
+	if randf() < EASTER_EGG_CHANCE:
+		_trigger_easter_egg()
+		return
+	
 	animation_player.play("play_fade_out")
 	animation_player.animation_finished.connect(
 		func(_anim):
@@ -55,3 +69,20 @@ func _on_play_button_pressed() -> void:
 			Game.room_list.load_room_infos()
 			get_tree().change_scene_to_file("uid://of56mmim7b8x")
 	)
+	
+	
+func _trigger_easter_egg() -> void:
+	ui.visible = false
+	ui.process_mode = Node.PROCESS_MODE_DISABLED
+	title.process_mode = Node.PROCESS_MODE_DISABLED
+	easter_egg_node.visible = true
+	easter_egg_audio_pos = menu_music_player.get_playback_position()
+	var timer := Timer.new()
+	timer.autostart = true
+	timer.wait_time = EASTER_EGG_AUDIO_LAG_LENGTH
+	timer.timeout.connect(
+		func():
+			menu_music_player.seek(easter_egg_audio_pos)
+	)
+	easter_egg_node.add_child(timer)
+	get_tree().create_timer(EASTER_EGG_DURATION).timeout.connect(get_tree().quit)
