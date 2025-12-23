0cd675af650354fd5985cb53f05fd016e37be5df
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-12-23 22:22:29 +0100

    Implement loot tables
    Implement item spawners
    Items are now randomly rotated when spawned

--- items/loot_tables/loot_table.gd
+++ items/loot_tables/loot_table.gd
@@ -3,3 +3,23 @@ extends Resource
 
 
 @export var table: Dictionary[BaseItemInfo, float]
+
+
+## Returns a random item based on this loot table
+## Returns null if no item was selected
+func poll() -> BaseItemInfo:
+	if table.is_empty():
+		Game.print_warning("Loot table is empty")
+		return null
+		
+	var sum: float = table.values().reduce(func(acc, val): return acc + val, 0)
+	var keys: Array[BaseItemInfo] = table.keys()
+	var selected: float = randf_range(0.0, sum)
+	var current: float = 0.0
+	
+	for key: BaseItemInfo in keys:
+		current += table[key]
+		if selected <= current:
+			return key
+			
+	return keys[-1]
--- items/on_ground/consumable_on_ground.gd
+++ items/on_ground/consumable_on_ground.gd
@@ -3,20 +3,9 @@ extends BaseItemOnGround
 
 
 @export var amount: int = 1
-## Manually sets the item to give when picked up
-## If empty, the consumable info should be set by the item spawner
-@export var item_id: StringName
+
 
 var consumable_info: ConsumableInfo
-
-
-func _ready() -> void:
-	super._ready()
-	if not item_id.is_empty():
-		if not Game.item_manager.consumable_infos.has(item_id):
-			Game.print_warning("Consumable ", item_id, " is manually set in ItemOnGround but not loaded")
-		else:
-			consumable_info = Game.item_manager.consumable_infos[item_id]
 
 
 func _on_pickup() -> void:
